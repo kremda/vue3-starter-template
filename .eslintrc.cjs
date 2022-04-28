31daa77a384130b35eb419950e8de8b9f2b316cc
Patrik <Patrik.Kremecek@gmail.com>
2022-04-28 17:09:42 +0200

    Updated eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
   'root': true,
   'extends': [
     'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript/recommended',
     './.eslintrc-auto-import.json',
